5bc3b94d924b7b91ecdadcc37d7f51e4231ff4f9
Prashant Rajput <prashant.rajput@volansys.com>
2023-02-24 17:00:44 +0530

    add circle image dependency and change version

---
+++ MavenDemoLib/src/main/java/com/mavendemolib/MavenPublishActivity.kt
@@ -0,0 +1,15 @@
+package com.mavendemolib
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mikhaellopez.circularimageview.CircularImageView
+
+class MavenPublishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maven_publish)
+        CircularImageView.let {
+
+        }
+    }
+}

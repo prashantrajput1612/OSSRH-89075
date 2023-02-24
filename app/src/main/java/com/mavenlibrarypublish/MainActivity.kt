977841c3d467254ed79a78bd4c9046162b952d41
Prashant Rajput <prashant.rajput@volansys.com>
2023-02-24 17:30:48 +0530

    minor change

--- app/src/main/java/com/mavenlibrarypublish/MainActivity.kt
+++ app/src/main/java/com/mavenlibrarypublish/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maven_publish)
+        setContentView(R.layout.activity_main)
         val text = findViewById<TextView>(R.id.textView)
         text.text= MavenCentralLibUtils.suburbs[0]
     }
